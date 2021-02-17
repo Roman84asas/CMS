47e8c46b76a2bc49153043e08dbe8d861c751abd
RomanMereneanu <asasin84@mail.ru>
2021-02-17 14:19:56 +0200

    add

--- project/bin/CMS/backend/models.py
+++ project/bin/CMS/backend/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+# Шаблоны body элемента
+class BodyTemplate(models.Model):
+    name = models.CharField(max_length=400)
+    temp_body_id = models.CharField(max_length=500)
+    text = models.TextField
+
+
+# Шаблоны div элементов
+class DivSTemplate(models.Model):
+    name = models.CharField(max_length=400)
+    temp_body_id = models.CharField(max_length=500)
+    header = models.TextField
+    text = models.TextField
+
+
+# Шаблоны страниц
+class HTMLTemplate(models.Model):
+    name = models.CharField(max_length=400)
+    use_name = models.CharField(max_length=500)
+    header = models.TextField
+    body_numbers = models.CharField(max_length=1000)
+
+
+# Шаблоны созданых Body элементов
