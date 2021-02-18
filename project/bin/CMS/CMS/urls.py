fa4b067d6c0cc8745e08d0d888413699ac1f0498
RomanMereneanu <asasin84@mail.ru>
2021-02-18 16:45:45 +0200

    add static dir

--- project/bin/CMS/CMS/urls.py
+++ project/bin/CMS/CMS/urls.py
@@ -20,4 +20,5 @@ from backend import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('htmles/', views.index_htmles),
 ]
--- project/bin/CMS/backend/views.py
+++ project/bin/CMS/backend/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Загрузачная страница с выводом ссылок на редактор существующих страницы
 def index(request):
     return render(request, 'index.html')
+
+
+def index_htmles(request):
+    return render(request, 'htmles/index.html')
