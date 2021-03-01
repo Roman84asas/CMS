76014bca51b2564f5c48e6d0ae51a0ad27b6a2fa
RomanMereneanu <asasin84@mail.ru>
2021-03-01 12:21:04 +0200

    add all

--- project/bin/CMS/backend/services.py
+++ project/bin/CMS/backend/services.py
@@ -1,3 +1,4 @@
+from .forms import FormBody
 from .models import BodyTemplate, DivSTemplate, HTMLTemplate
 
 
@@ -6,6 +7,19 @@ def return_all_object():
     dives = DivSTemplate.objects.all()
     htmles = HTMLTemplate.objects.all()
     return body, dives, htmles
+
+
+def return_body_form(name, name_id, all_body):
+    form_body = FormBody()
+    form_body.initial["name"] = name
+    form_body.initial["name_id"] = name_id
+    form_body.initial["all_body"] = all_body
+    return form_body
+
+
+def create_body(name, name_id, all_body):
+    body_template = BodyTemplate(name=name, temp_body_id=name_id, text=all_body)
+    body_template.save()
 
 
 def return_body_object(elementid):
--- project/bin/CMS/backend/views.py
+++ project/bin/CMS/backend/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Загрузачная страница с выводом ссылок на редактор существующих страницы
 from backend.forms import FormBody, FormDives
 from .models import BodyTemplate, DivSTemplate
-from .services import return_all_object, return_body_object, return_dives_object, return_new_body
+from .services import return_all_object, return_body_object, return_dives_object, return_new_body, return_body_form, \
+    create_body
 
 
 def index(request):
@@ -23,48 +24,57 @@ def create_htmles(request):
 
 # Боди элементы и методы работы с ними
 def index_bodys(request, elementid):
+    # Возвращает все элементы из базы данных для виджета
     body, dives, htmles = return_all_object()
+
     if request.method == 'POST':
         name = request.POST.get('name')
         name_id = request.POST.get('name_id')
         all_body = request.POST.get('all_body')
+
+        # Обновляет данные BODY секции
         return_new_body(elementid, name, name_id, all_body)
-        form_body = FormBody()
-        form_body.initial["name"] = name
-        form_body.initial["name_id"] = name_id
-        form_body.initial["all_body"] = all_body
+
+        # Возвращает форму для BODY секции
+        form_body = return_body_form(name, name_id, all_body)
+
         return render(request, 'bodys/index.html',
                       {"form": form_body, 'bodys': body, 'dives': dives, 'htmles': htmles})
     else:
+        # Возвращает данные BODY секции
         name, name_id, all_body = return_body_object(elementid)
-        form_body = FormBody()
-        form_body.initial["name"] = name
-        form_body.initial["name_id"] = name_id
-        form_body.initial["all_body"] = all_body
+
+        # Возвращает форму для BODY секции
+        form_body = return_body_form(name, name_id, all_body)
+
         return render(request, 'bodys/index.html',
                       {"form": form_body, 'bodys': body, 'dives': dives, 'htmles': htmles})
 
 
 def create_bodys(request, elementid):
+    # Возвращает все элементы из базы данных для виджета
     body, dives, htmles = return_all_object()
+
     if request.method == 'POST':
         name = request.POST.get('name')
         name_id = request.POST.get('name_id')
         all_body = request.POST.get('all_body')
-        body_template = BodyTemplate(name=name, temp_body_id=name_id, text=all_body)
-        body_template.save()
-        form_body = FormBody()
-        form_body.initial["name"] = name
-        form_body.initial["name_id"] = name_id
-        form_body.initial["all_body"] = all_body
+
+        # Создает данные BODY секции
+        create_body(name, name_id, all_body)
+
+        # Возвращает форму для BODY секции
+        form_body = return_body_form(name, name_id, all_body)
+
         return render(request, 'bodys/create.html',
                       {"form": form_body, 'bodys': body, 'dives': dives, 'htmles': htmles})
     else:
+        # Возвращает данные BODY секции
         name, name_id, all_body = return_body_object(elementid)
-        form_body = FormBody()
-        form_body.initial["name"] = name
-        form_body.initial["name_id"] = name_id
-        form_body.initial["all_body"] = all_body
+
+        # Возвращает форму для BODY секции
+        form_body = return_body_form(name, name_id, all_body)
+
         return render(request, 'bodys/create.html',
                       {"form": form_body, 'bodys': body, 'dives': dives, 'htmles': htmles})
 
@@ -72,7 +82,9 @@ def create_bodys(request, elementid):
 # Див элементы и методы работы с ними
 def index_dives(request, elementid):
     name, name_id, title_body, all_body = return_dives_object(elementid)
+    # Возвращает все элементы из базы данных для виджета
     body, dives, htmles = return_all_object()
+    # Возвращает форму для DIV секции
     form_dives = FormDives()
     form_dives.initial["name"] = name
     form_dives.initial["name_id"] = name_id
@@ -83,6 +95,7 @@ def index_dives(request, elementid):
 
 def create_dives(request, elementid):
     name, name_id, title_body, all_body = return_dives_object(elementid)
+    # Возвращает все элементы из базы данных для виджета
     body, dives, htmles = return_all_object()
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -91,6 +104,7 @@ def create_dives(request, elementid):
         all_body = request.POST.get('all_body')
         divs_template = DivSTemplate(name=name, temp_body_id=name_id, header=title_body, text=all_body)
         divs_template.save()
+        # Возвращает форму для DIV секции
         form_dives = FormDives()
         form_dives.initial["name"] = name
         form_dives.initial["name_id"] = name_id
@@ -99,6 +113,7 @@ def create_dives(request, elementid):
         return render(request, 'dives/create.html',
                       {"form": form_dives, 'bodys': body, 'dives': dives, 'htmles': htmles})
     else:
+        # Возвращает форму для DIV секции
         form_dives = FormDives()
         form_dives.initial["name"] = name
         form_dives.initial["name_id"] = name_id
