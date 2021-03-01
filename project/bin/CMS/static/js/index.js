4ebbf4d7a219283256d4769eed277ce12d41a2d6
RomanMereneanu <asasin84@mail.ru>
2021-03-01 12:46:25 +0200

    add all

--- project/bin/CMS/static/js/index.js
+++ project/bin/CMS/static/js/index.js
@@ -6,40 +6,61 @@
    itemBody.forEach(function (element) {
        element.addEventListener('click', function (){
            const valueData = element.getAttribute('data-bodys');
+
            const hrefBody = document.querySelector('#bodys');
            const hrefUpd = document.querySelector('#bodys-update');
+           const delBody = document.querySelector('#del-bodys');
+
            const allAddress = '/bodys/create/'+valueData
            const allAddressUp = '/bodys/'+valueData
+
            hrefBody.removeAttribute('href');
            hrefBody.setAttribute('href', allAddress);
+
            hrefUpd.removeAttribute('href');
            hrefUpd.setAttribute('href', allAddressUp);
+
+           delBody.setAttribute('data-bodys', valueData);
        })
    })
     itemDive.forEach(function (element) {
        element.addEventListener('click', function (){
            const valueData = element.getAttribute('data-dive');
+
            const hrefBody = document.querySelector('#dives');
            const hrefUpd = document.querySelector('#dives-update');
+           const delBody = document.querySelector('#del-dive');
+
            const allAddress = '/dives/create/'+valueData
            const allAddressUp = '/dives/'+valueData
+
            hrefBody.removeAttribute('href');
            hrefBody.setAttribute('href', allAddress);
+
            hrefUpd.removeAttribute('href');
            hrefUpd.setAttribute('href', allAddressUp);
+
+           delBody.setAttribute('data-dive', valueData);
        })
    })
     itemHtml.forEach(function (element) {
        element.addEventListener('click', function (){
            const valueData = element.getAttribute('data-htmle');
+
            const hrefBody = document.querySelector('#htmles');
            const hrefUpd = document.querySelector('#htmles-update');
+           const delBody = document.querySelector('#del-htmle');
+
            const allAddress = '/htmles/create/'+valueData
            const allAddressUp = '/htmles/'+valueData
+
            hrefBody.removeAttribute('href');
            hrefBody.setAttribute('href', allAddress);
+
            hrefUpd.removeAttribute('href');
            hrefUpd.setAttribute('href', allAddressUp);
+
+           delBody.setAttribute('data-htmle', valueData);
        })
    })
 })()
