9f24df4ed145f6e7718259147259f2caf7101e18
RomanMereneanu <asasin84@mail.ru>
2021-03-02 12:12:56 +0200

    add all

--- project/bin/CMS/static/js/htmles.js
+++ project/bin/CMS/static/js/htmles.js
@@ -1,14 +1,32 @@
 (function () {
-   const addDiv = document.querySelector('#add_div');
-   const close = document.querySelector('#close');
+    const addDiv = document.querySelector('#add_div');
+    const close = document.querySelector('#close');
+    const dataSp = document.querySelectorAll('.data_sp');
+    const selectSp = document.querySelectorAll('.select_sp');
 
-   addDiv.addEventListener('click', function (){
-       const popup = document.querySelector('#hidden_popup');
-       popup.style.display = 'block';
-   })
+    addDiv.addEventListener('click', function (){
+        const popup = document.querySelector('#hidden_popup');
+        popup.style.display = 'block';
+    })
 
-   close.addEventListener('click', function (){
-       const popup = document.querySelector('#hidden_popup');
-       popup.style.display = 'none';
-   })
+    close.addEventListener('click', function (){
+        const popup = document.querySelector('#hidden_popup');
+        popup.style.display = 'none';
+    })
+    dataSp.forEach(function (element) {
+        element.addEventListener('click', function () {
+            dataSp.forEach(function (element) {
+                element.classList.remove('active')
+            })
+            element.classList.add('active')
+        })
+    })
+    selectSp.forEach(function (element) {
+        element.addEventListener('click', function () {
+            selectSp.forEach(function (element) {
+                element.classList.remove('active')
+            })
+            element.classList.add('active')
+        })
+    })
 })()
