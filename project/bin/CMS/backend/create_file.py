ceb0d29f89bf04d76b5b06630e42c94996b21aa8
RomanMereneanu <asasin84@mail.ru>
2021-03-03 08:39:19 +0200

    add all

--- project/bin/CMS/backend/create_file.py
+++ project/bin/CMS/backend/create_file.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from backend.shablon import select_template_create
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = Path.joinpath(BASE_DIR, "production")
 
 
 def create_file(use_name, items):
+    all_file = []
     for item in items:
-        print(item['data_id'])
+        section = select_template_create(item)
+        all_file.append(section)
--- project/bin/CMS/backend/shablon.py
+++ project/bin/CMS/backend/shablon.py
@@ -1,3 +1,8 @@
-def select_template(item):
+def select_template_create(item):
     if item['data_id'] == '111111':
         return item['data_text']
+
+    if item['data_id'] == '222222':
+        return item['data_text']
+
+    print(item['data_text'])
